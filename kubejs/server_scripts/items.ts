19855dbf062feba4a1d54cc9b268e76ef06cb4ac
HoY <hoyin1600p@gmail.com>
2025-03-31 07:54:47 -0600

    fix wirts

--- kubejs/server_scripts/items.ts
+++
@@ -1,6 +0,0 @@
-StartupEvents.registry('item', event => {
-  event.create('wirts_leg')
-    .displayName("Wirt's Leg")
-    .tooltip('Moooo')
-    .rarity('epic') // options: common, uncommon, rare, epic
-})
