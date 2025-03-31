302e943a8a338226d064d2c91257fca2f12417d0
HoY <hoyin1600p@gmail.com>
2025-03-30 22:28:53 -0600

    add cow stuff

--- kubejs/server_scripts/items.js
+++ kubejs/server_scripts/items.js
@@ -1,6 +1,6 @@
 StartupEvents.registry('item', event => {
   event.create('wirts_leg')
-    .displayName('Wirt's Leg)
+    .displayName("Wirt's Leg")
     .tooltip('Moooo')
     .rarity('epic') // options: common, uncommon, rare, epic
 })
